501caea2a726d70ef4c3b435609bd635be405670
Jim J Galloway <pmi.mobile@gmail.com>
2019-08-22 17:03:58 -0700

    Add a test sequence.

--- autowater.py
+++ autowater.py
@@ -1,2 +1,11 @@
 import time
 import piplates.RELAYplate as plate
+
+plate.relayON(0,3)
+plate.relayON(0,7)
+time.sleep(10)
+plate.relayOFF(0,3)
+plate.relayON(0,4)
+time.sleep(7)
+plate.relayOFF(0,7)
+plate.relayOFF(0,4)
