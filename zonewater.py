f143f908e756cca11b6648020bb0472dd2c5dde9
Jim J Galloway <pmi.mobile@gmail.com>
2019-08-27 17:22:57 -0700

    Created single zone control application.

---
+++ zonewater.py
@@ -0,0 +1,19 @@
+import argparse
+import time
+import piplates.RELAYplate as plate
+
+parser = argparse.ArgumentParser(description='Turn on specific zone for a specified interval.')
+parser.add_argument('-z', '--zone', action='store_const', type=int, help='specify zone', choices=range(1,6), required=True)
+parser.add_argument('-t', '--time', action='store_const', type=int, help='specify interval', choices=range(1,31), default=15)
+args = parser.parse_args()
+
+# Turn on the relay that powers the pump
+plate.relayON(0,7)
+
+# Turn on specified relay for a specified/default interval
+plate.relayON(0,args.zone)
+time.sleep(args.time*60)
+plate.relayOFF(0,args.zone)
+
+# Turn off the relay that powers the pump
+plate.relayOFF(0,7)
